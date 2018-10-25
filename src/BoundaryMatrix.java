495880b8c72b11f2df8e7ff2682434feae8b9ad2
wafonro <wagner.fonseca.rodrigues@gmail.com>
2018-10-25 20:27:20 +0200

    Made boundary matrix more lisible

--- src/BoundaryMatrix.java
+++ src/BoundaryMatrix.java
@@ -6,34 +6,33 @@ import java.util.LinkedHashMap;
 
 public class BoundaryMatrix {
        int[][] matrix;
-       //Vector<Integer> lowsList=initialLows();
-       Vector<Integer> pivotsToFind;
+       Vector<Integer> columnsWithoutPivot;
        Vector<Integer> pivotsFound;
-       
-       
-       Vector<LinkedList<Integer>> sparseMatrix;
-       Vector<LinkedList<Integer>> sparseMatrixR;
-       
-       
-       public BoundaryMatrix(int N, Vector<Simplex> F) {
-    	   matrix=new int [N][N];
-    	   pivotsToFind=new Vector<Integer>();
+	   
+	   // creates the full boundary matrix given one simplex
+       public BoundaryMatrix(Vector<Simplex> F) {
+		   int N = F.size();
+		   matrix=new int [N][N];
+    	   columnsWithoutPivot=new Vector<Integer>();
     	   pivotsFound=new Vector<Integer>();
+		   
+		   // initialization
     	   for(int i=0; i<N; i++) {
-    		   for(int j=0; j<N;j++) 
-    			   matrix[i][j]=0;
-    		   
-    		   //pivotsToFind=new Vector<Integer>();
-    		   pivotsToFind.add(i);
+    		   for(int j=0; j<N;j++){
+				   matrix[i][j]=0;
+				} 
+    		   columnsWithoutPivot.add(i);
     		   pivotsFound.add(null);
-    		   //pivotsFound=new Vector<Integer>();
     	   }
     	   
     	   for(int i=0; i<N; i++) {
-    		   int dimRow=F.get(i).dim;
-    		   for(int j=i+1; j<N; j++) {
+			   int dimRow=F.get(i).dim;
+			   
+			   for(int j=i+1; j<N; j++) {
     			   int dimColumn=F.get(j).dim;
-    			   if(dimColumn==dimRow+1) {
+				   // boundary simplex has simplexdimension - 1
+				   if(dimColumn==dimRow+1) {
+
     			   boolean containsAllVertexes=true;
     			   for(Integer vertex: F.get(i).vert) {
     				   if(!F.get(j).vert.contains(vertex)) {
@@ -46,75 +45,21 @@ public class BoundaryMatrix {
     			   }
     			 }  
     		   }
-    		   System.out.println("i= "+i);
     	   }
     	   
        }
-       
-       public BoundaryMatrix(Vector<Simplex> F) {
-    	   sparseMatrix=new Vector<LinkedList<Integer>>();
-    	   sparseMatrixR=new Vector<LinkedList<Integer>>();
-    	   int N=F.size();
-    	   for(int i=0; i<N; i++) {
-    		   sparseMatrix.add(new LinkedList<Integer>());
-    		   sparseMatrixR.add(new LinkedList<Integer>());
-    	   }
-    	   for(int i=0; i<N; i++) {
-    		   int dimRow=F.get(i).dim;
-    		   for(int j=i+1; j<N; j++) {
-    			   int dimColumn=F.get(j).dim;
-    			   if(dimColumn==dimRow+1) {
-    			   boolean containsAllVertexes=true;
-    			   for(Integer vertex: F.get(i).vert) {
-    				   if(!F.get(j).vert.contains(vertex)) {
-    					   containsAllVertexes=false;
-    					   break;
-    				   }
-    			   }
-    			   if(containsAllVertexes) {
-
-    				   sparseMatrix.get(j).add(0,i);
-    				   sparseMatrixR.get(j).add(0,i);
-    				   
-    			   }
-    			 }  
-    		   }
-    	      System.out.println("i= "+i);
-    		  
-    	   }
-    	   //for(int i=0; i<N; i++) {
-    		   //if(!sparseMatrix.get(i).isEmpty()) {
-    			   //System.out.println(sparseMatrix.get(i));
-    		   //}
-    	   //}
-    }
-       
-       public Vector<Integer> initialLows(){
-    	   Vector<Integer> v= new Vector<Integer>();
-    	   for(int j=0; j<matrix.length; j++) {
-    		   int low=-1;
-    		   for(int i=0; i<matrix.length; i++) {
-    			   if(matrix[i][j]!=0) {
-    				   low=i;
-    			   }
-    		   }
-    		   //v.set(j, low);
-    		   v.add(j,low);
-    	   }
-    	   
-    	   return v;
-       }
-       
-       
        public void Reduction() {
+		   // iterate from bottom to up and find pivots
+		   // pivot is the leftmost true in the line
+		   // sum pivot column to all columns that have true in the line
     	   int row = matrix.length-1;
     	   while (row>=0) {
-    		   Vector<Integer> lows =equalLowsList(row);
+    		   Vector<Integer> lows = possiblePivots(row);
     		   if(lows.size()>=1) {
+				   // pivot is the first because its the leftmost
     			   int left=lows.get(0);
     			   pivotsFound.set(left,row);
     			   lows.remove(0);
-    			   if(lows.size()>0) {
     			   for(int j: lows) {
     				   double factor=matrix[row][j]/matrix[row][left];
     				   for(int i=row; i>=0;i--) {
@@ -124,18 +69,13 @@ public class BoundaryMatrix {
     					   }
     				   }
     			   }
-    			  } 
-    			   boolean remove= pivotsToFind.removeElement( left);
+    			   boolean remove= columnsWithoutPivot.removeElement( left);
     		   }
-    		   
-    		   System.out.println("j= "+row);
-    		   
     		   row--;
-    		   
-    		   
     	   }
        }
-       
+	   
+	   // algorithm seem on course
        public void barCode(Vector<Simplex> F) {
     	   String lines="";
     	   for (int j=0; j<matrix.length; j++) {
@@ -149,29 +89,14 @@ public class BoundaryMatrix {
     				   lines=lines+value_j;
     				   System.out.println(lines);
     				   lines="";
-    				   
-        			   
     			   }
-    			   
     		   }
     		   else {
     			   boolean rightValueFound=false;
     			   for(int k=j+1; k<matrix.length; k++) {
     				   if(pivotsFound.get(k)!=null&&pivotsFound.get(k)==j) {
     					   rightValueFound=true;
-    					   //float leftValue=F.get(j).val;
-    					   //float rightValue=F.get(k).val;
-    					   //if(leftValue!=rightValue) {
-    	    				   //lines=lines+F.get(j).dim+" ";
-    	    				   //lines=lines+leftValue+" ";
-    	    				   //lines=lines+rightValue;
-    	    				   //System.out.println(lines);
-    	    				   //lines="";
-    	    				   
-    	        			   
-    	    			   //}
     					   break;
-    					   
     				   }
     			   }
     			   if(!rightValueFound) {
@@ -183,130 +108,20 @@ public class BoundaryMatrix {
     				   lines="";
     			   }
     		   }
-    		   
-    		   
-    		   
-    		   System.out.println("k= "+j);
-    		   
     	   }
        }
        
-       public Vector<Integer> equalLowsList(int row){
+       public Vector<Integer> possiblePivots(int row){
     	   Vector<Integer> v= new Vector<Integer>();
     	   
-    	   for(Integer column: pivotsToFind) {
+    	   for(Integer column: columnsWithoutPivot) {
     		   if(matrix[row][column]!=0) {
     			   v.add(column);
     		   }
     	   }
     	   return v;
        }
-       
-       
-       public LinkedList<Integer> merge(LinkedList<Integer> l1, LinkedList<Integer> l2){
-    	   LinkedList<Integer> list=new LinkedList<Integer>();
-    	   int size1= l1.size();
-    	   int size2= l2.size();
-    	   int i1=0;
-    	   int i2=0;
-    	   Vector<Integer> toRemove=new Vector<Integer>();
-    	   while(i1<size1||i2<size2) {
-    		   if(i1>=size1) {
-    			   list.add(l2.get(i2));
-    			   //toRemove.add(i2);
-    			   i2++;
-    		   }
-    		   else if(i2>=size2) {
-    			   list.add(l1.get(i1));
-    			   i1++;
-    		   }
-    		   else if(l1.get(i1)>l2.get(i2)) {
-    		       list.add(l1.get(i1));
-    		       i1++;   
-    		   }
-    		   else if(l2.get(i2)>l1.get(i1)) {
-    			   list.add(l2.get(i2));
-    			   //toRemove.add(i2);
-    		       i2++;   
-    		   }
-    		   // equalitity
-    		   else {
-    			   i1++;
-    			   //toRemove.add(i2);
-    			   i2++;
-    		   }
-    		   
-
-    	   
-           }
-    	   for(int i=toRemove.size()-1; i>=0; i--) {
-    		   l2.remove((int) toRemove.get(i));
-    	   }
-    	   return list;
-       }
-       
-       
-       public LinkedHashMap<Integer, Integer> sparseReduction(Vector<Simplex> F) {
-    	   LinkedHashMap<Integer, Integer> mapBirthDeath=new LinkedHashMap<>();
-    	   for(int j=0; j<F.size();j++) {
-    		   LinkedList<Integer> L= (LinkedList<Integer>) sparseMatrix.get(j).clone();
-    		   //System.out.println("sparse");
-    		   //for(LinkedList<Integer> ls: sparseMatrix) {
-    			   //System.out.println(ls);
-    		   //}
-    		   sparseMatrixR.set(j,new LinkedList<Integer>());
-    		   while((L!=null&&L.size()>0)&&!sparseMatrixR.get(L.get(0)).isEmpty()) {
-    			   //if(!sparseMatrixR.get(L.get(0)).isEmpty()) {
-    			      L=merge(L,sparseMatrixR.get(L.get(0)));
-    			   //}
-    			   
-    		   }
-    		   
-    		   
-    		   
-    		   if(L!=null&&L.size()>0) {
-    			   if(F.get(j).val>F.get(L.get(0)).val){
-    				   mapBirthDeath.put(L.get(0),j);
-    			   }
-    			   else {
-    				   mapBirthDeath.remove(L.get(0));
-    			   }
-    			   sparseMatrixR.set(L.get(0),L);
-    		   }
-<<<<<<< HEAD
-    		   //System.out.println("decimacol");
-    		   //System.out.println(sparseMatrixR.get(8));
-    		   
-    	   }
-    	   for(int i=0; i<sparseMatrixR.size();i++) {
-    		   if(sparseMatrixR.get(i)!=null&&sparseMatrixR.get(i).size()>0) {
-    			   System.out.println(i);
-    			   System.out.println(sparseMatrixR.get(i));
-=======
-    		   else {
-    			   mapBirthDeath.put(j,-1);
->>>>>>> ab39ffa93fe797b3a6ec0354de81d8730df0df45
-    		   }
-    		   //System.out.println("decimacol");
-    		   //System.out.println(sparseMatrixR.get(8));
-    		   
-    		   System.out.println("j= "+j);
-    		   
-    	   }
-    	   
-    	   //System.out.println(mapBirthDeath);
-    	   return mapBirthDeath;
-    	   //for(int i=0; i<sparseMatrixR.size();i++) {
-    		   //if(sparseMatrixR.get(i)!=null&&sparseMatrixR.get(i).size()>0) {
-    			   //System.out.println(i);
-    			   //System.out.println(sparseMatrixR.get(i));
-    		   //}
-    		   
-    	   //}
-       }
-       
-       
-       
+	   
        public String toString() {
     	   int n = matrix.length;
     	   String m ="";
